52a37ce79ba77e4dd86cbccd17d3ca6747c0dd1f
Matthew Simo <matthew.a.simo@gmail.com>
2014-03-12 22:27:29 -0500

    Add admin starter templates. Add admin routes to app.js

--- client/app.js
+++ client/app.js
@@ -7,17 +7,41 @@ Router.configure({
 
 Router.map(function () {
 
+  /*
+   * Client Routes */
+  
   this.route('home', {
     path: '/',
-  });
-
-  this.route('admin', {
-    path: '/admin',
   });
 
   this.route('posts', {
     path: '/posts',
   });
+
+
+  /*
+   * Admin Routes */
+
+  this.route('admin', {
+    path: '/admin',
+  });
+
+  this.route('admin-new', {
+    path: '/admin/new',
+  });
+
+  this.route('admin-posts', {
+    path: '/admin/posts',
+  });
+
+  this.route('admin-pages', {
+    path: '/admin/pages',
+  });
+  
+  this.route('admin-settings', {
+    path: '/admin/settings',
+  });
+
 
 });
 
